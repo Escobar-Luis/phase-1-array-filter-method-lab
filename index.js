2ad1e31d94255806cd03974bb2dde8239f55dbab
Luis Escobar <lae311@stern.nyu.edu>
2021-10-15 15:43:23 -0400

    commit tree 76c15a683c9f93191830a04d43d5118a0bdb41f3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const result = drivers.filter(function(n) {
+        if (n.toLowerCase() === string.toLowerCase()) {
+            return string
+        }
+    })
+    return result
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(function(name) {
+        if (string.substring(0, 2) === name.substring(0, 2))
+            return name
+    })
+    return result
+}
+
+function matchName(drivers, string) {
+    const result = drivers.filter(function(names) {
+        if (names['name'] === string) {
+            return names
+        }
+    })
+    return result
+}
+
+
+
+
+
